1180d579843d45a163685236a62446f591792384
LiamWalenius <liamwalenius@gmail.com>
2024-04-28 20:51:05 -0400

    fix merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
-import Fortnite from "./components/Fortnite.tsx"
 
 function App() {
   return (
     <div className="App">
-        <Fortnite/>
         <button className="App-button">Some cool things will happen when you click me!</button> {/* Adding a button */}
     </div>
   )
